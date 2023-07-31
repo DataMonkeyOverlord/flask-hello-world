7c60f3e1bacaec9a678f615f506ad7629d8d0372
Abdullah Adlouni <abad2907@colorado.edu>
2023-07-31 10:35:03 +0000

    changed with ecternal use

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def hello_world():
 
 @app.route('/db_test')
 def testing():
-    conn = psycopg2.connect("postgres://bh_123_user:9UbstWz5lrwnwNuF62Xr8qHD2jjHH0rj@dpg-cj232pc07spkp67l4cqg-a/bh_123")
+    conn = psycopg2.connect("postgres://bh_123_user:9UbstWz5lrwnwNuF62Xr8qHD2jjHH0rj@dpg-cj232pc07spkp67l4cqg-a.oregon-postgres.render.com/bh_123")
     conn.close()
     return 'Database Connection Sucessful!'
 
